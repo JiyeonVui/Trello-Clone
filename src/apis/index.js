20c895410d5386d10e433a3fdcecf434f3170c6d
manhnq <manh.nq0508@gmail.com>
2025-04-22 17:22:39 +0700

    keo api

---
+++ src/apis/index.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+import { API_ROOT } from '~/utils/constants'
+
+/**
+ * Lưu ý: Đối với việc sử dụng axios ở khóa MERN STACK PRO
+ * Tất cả các function bên dưới các bạn sẽ thấy mình chỉ request và lấy data luôn mà ko có try catch hay then catch gì để bắt lỗi
+ * Lý do là vì ở phía Front-end chúng ta ko cần thiết phải làm như vậy đối với mọi request bởi nó sẽ gây ra việc dư thừa code catch lỗi
+ * quá nhiều.
+ * Giải pháp Clean code đó là chúng ta sẽ tập trung catch lỗi tập trung ở 1 nới bằng cách tận dụng 1 thứ cực kỳ mạnh mẽ trong axios đó là Interceptor
+ * Hiểu đơn giản Interceptor là cách mà chúng ta đánh chặn giữa request hoặc response để xử lý logic mà chúng ta muốn.
+ * Sẽ có ở Mern Stack advanced khóa học sau.
+ */
+
+export const fetchBoardDetailsAPI = async (boardId) => {
+  const response = await axios.get(`${API_ROOT}/v1/boards/${boardId}`)
+  console.log('response', response.data)
+  return response.data
+}
+
--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -2,16 +2,28 @@ import Container from '@mui/material/Container'
 import AppBar from '~/components/AppBar'
 import BoardBar from '~/pages/Boards/BoardBar'
 import BoardContent from '~/pages/Boards/BoardContent/BoardContent'
-import { mockData } from '~/apis/mock-data'
+import { useEffect, useState } from 'react'
+import { fetchBoardDetailsAPI } from '~/apis'
 
 function Board() {
+  const [board, setBoard] = useState(null)
+
+  useEffect(() => {
+    // "Tam thời fix cứng boardId, flow chuẩn chỉnh về sau khi học khóa nâng cao sẽ dùng react-router-dom để lấy boardId từ url"
+    const boardId = '68076aedebfa334f1976ac09'
+    // call api
+    fetchBoardDetailsAPI(boardId).then((board) => {
+      setBoard(board)
+    })
+  }, [])
+
   return (
     <Container disableGutters maxWidth={false} sx={{
       height: '100vh'
     }}>
       <AppBar />
-      <BoardBar board={mockData?.board} />
-      <BoardContent board={mockData?.board} />
+      <BoardBar board={board} />
+      <BoardContent board={board} />
       {/* <VideoPlayer /> */}
     </Container>
   )
