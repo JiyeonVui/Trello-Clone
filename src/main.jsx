4c91b804f3038c34c78798cbcd57ff3c66de0775
Jiyeon <manh.nq0508@gmail.com>
2025-05-14 15:31:20 +0700

    config redux

--- src/main.jsx
+++ src/main.jsx
@@ -9,21 +9,23 @@ import theme from './theme.js'
 import { ToastContainer } from 'react-toastify'
 import { ConfirmProvider } from 'material-ui-confirm'
 import 'react-toastify/dist/ReactToastify.css'
+import { store } from './redux/store'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <CssVarsProvider theme={theme}>
-    <ConfirmProvider defaultOptions={{
-      allowClose: false,
-      buttonOrder: ['confirm', 'cancel'],
-      dialogProps: { maxWidth: 'xs' },
-      cancellationButtonProps: { color: 'inherit' },
-      confirmationButtonProps: { color: 'secondary', variant: 'outlined' }
-    }}>
-      <CssBaseline />
-      <App />
-      <ToastContainer position='bottom-left' />
-    </ConfirmProvider>
-  </CssVarsProvider>
-  // </React.StrictMode>
+  <Provider store={store}>
+    <CssVarsProvider theme={theme}>
+      <ConfirmProvider defaultOptions={{
+        allowClose: false,
+        buttonOrder: ['confirm', 'cancel'],
+        dialogProps: { maxWidth: 'xs' },
+        cancellationButtonProps: { color: 'inherit' },
+        confirmationButtonProps: { color: 'secondary', variant: 'outlined' }
+      }}>
+        <CssBaseline />
+        <App />
+        <ToastContainer position='bottom-left' />
+      </ConfirmProvider>
+    </CssVarsProvider>
+  </Provider>
 )
